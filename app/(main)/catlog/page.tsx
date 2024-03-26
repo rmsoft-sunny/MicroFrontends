bb6a5ac492eb735b9bf92bb64fecdfa486f2c0e2
정은선 <jes@rmsoft.kr>
2024-03-26 16:48:00 +0900

    fix

---
+++ app/(main)/catlog/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Catlog() {
+  return <div>Catlog</div>;
+}
+
+export default Catlog;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewites() {
+    return [];
+  },
+};
 
 export default nextConfig;

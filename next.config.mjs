d8c50fc2de25e8aded320c658100fd5fc0d092d3
정은선 <jes@rmsoft.kr>
2024-05-07 11:28:10 +0900

    fix: config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -25,32 +25,32 @@ const nextConfig = {
       {
         basePath: false,
         source: "/api/login",
-        destination: `${process.env.NEXT_PUBLIC_CLIENT}/client/v1/api/auth/login`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/client/v1/api/auth/login`,
       },
       {
         basePath: false,
         source: "/api/logout",
-        destination: `${process.env.NEXT_PUBLIC_CLIENT}/client/v1/api/auth/logout`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/client/v1/api/auth/logout`,
       },
       {
         basePath: false,
         source: "/api/client/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CLIENT}/client/v1/api/:path*`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/client/v1/api/:path*`,
       },
       {
         basePath: false,
         source: "/api/project/:code/:path*",
-        destination: `${process.env.NEXT_PUBLIC_PROJECT}/:code/prj/v1/api/:path*`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/:code/prj/v1/api/:path*`,
       },
       {
         basePath: false,
         source: "/api/contents/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CONTENTS}/contents/v1/api/:path*`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/contents/v1/api/:path*`,
       },
       {
         basePath: false,
         source: "/api/finance/:path*",
-        destination: `${process.env.NEXT_PUBLIC_FINANCE}/finance/v1/api/:path*`,
+        destination: `http://dev-rm-seoul.iptime.org:32000/finance/v1/api/:path*`,
       },
     ];
   },
